0fbd779489f2f657ff9f3fec6fbd069fdd8554ca
c3ngo <faruk._yazici@outlook.com>
2023-09-06 03:28:57 +0300

    searchbar"none"gd

--- js/searchbar.js
+++ js/searchbar.js
@@ -3,12 +3,23 @@ document.getElementById("search-button").addEventListener("click", function() {
 
    var machineElements = document.querySelectorAll(".Machines .Machines-box");
 
+   var resultsFound = false; // Sonuç bulunup bulunmadığını kontrol etmek için bir değişken
+
    machineElements.forEach(function(machine) {
       var machineName = machine.querySelector("h3").textContent.toLowerCase();
       if (machineName.includes(searchTerm)) {
          machine.style.display = "block"; // Eşleşenleri göster
+         resultsFound = true; // Sonuç bulunduğunu işaretlemek için
       } else {
          machine.style.display = "none"; // Eşleşmeyenleri gizle
       }
    });
+
+   // Sonuç bulunmadıysa bir mesaj göster
+   var noResultsMessage = document.getElementById("no-results-message");
+   if (!resultsFound) {
+      noResultsMessage.style.display = "block";
+   } else {
+      noResultsMessage.style.display = "none";
+   }
 });
